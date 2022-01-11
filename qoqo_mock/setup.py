8359afb04304262e1d2df41e4d7d5e2d0d6e7499
kbarkhqs <65908581+kbarkhqs@users.noreply.github.com>
2022-01-11 09:47:41 +0100

    Updated dependencies (#33)
    * Fixed documentation
    
    * Removed mocked imports for qoqo_mock/qoqo_mock docs
    
    * Updated all packages to use the pyo3 0.15.0 versions
    
    * Updated changelog
    
    * Increased python version number
    
    * Updated dependencies
    
    * Updated version numbers for release

--- qoqo_mock/setup.py
+++ qoqo_mock/setup.py
@@ -27,7 +27,7 @@ with open(os.path.join(path, 'qoqo_mock/__version__.py')) as f:
 __version__ = lines[-1].strip().split("'")[1].strip()
 
 install_requires = [
-    'qoqo>=0.8.2',
+    'qoqo>=0.9.0',
     'numpy',
 ]
 
